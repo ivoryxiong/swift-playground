c175ae0e6caef182eee3dc03ee6b297829254f88
ivoryxiong <ivoryxiong@gmail.com>
2017-06-09 00:09:38 +0800

    import swift playground

---
+++ Swift-CodeSnap.playground/Contents.swift
@@ -0,0 +1,82 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+//MARK: utils method about sequence
+class SequenceUtils {
+    //O(n) 生产0到n-1的序列，满足条件: 位于i位置的值不为i
+    static func randSeq(_ n : Int) -> [Int] {
+        var seq = Array.init((0..<n))
+        for i in (0..<n).reversed() {
+            let next = Int(arc4random_uniform(UInt32(i)))
+            let tmp = seq[next]
+            seq[next] = seq[i]
+            seq[i] = tmp
+        }
+        return seq
+    }
+}
+
+class BigInteger {
+    var digits : [Int] = []
+
+    init(_ s : String) {
+        digits = s.characters.reversed().map({
+                c in
+                    if let digit = Int(String(c)) {
+                        return digit
+                    }
+                    return 0
+        })
+    }
+    
+    static func mul(a : BigInteger, b : BigInteger) -> String {
+        let possibleLen = a.digits.count + b.digits.count + 1
+        var ans = Array.init(repeating: 0, count: possibleLen)
+        for i in 0..<a.digits.count {
+            for j in 0..<b.digits.count {
+                ans[i+j] += a.digits[i] * b.digits[j]
+            }
+        }
+        for i in 0..<possibleLen {
+            if i < possibleLen - 1 {
+                ans[i+1] += ans[i] / 10
+            }
+            ans[i] = ans[i] % 10
+        }
+        var len = possibleLen
+        while(len>0 && ans[len-1] == 0) {
+            len = len-1
+        }
+
+        let ret = ans.prefix(len).reversed().reduce("", {$0 + String($1)})
+        return ret
+    }
+    
+    static func sum(a : BigInteger, b : BigInteger) -> String {
+        let possibleLen = max(a.digits.count , b.digits.count) + 1
+        var ans = Array.init(repeating: 0, count: possibleLen)
+        for i in 0..<possibleLen {
+            if i < a.digits.count {
+                ans[i] += a.digits[i]
+            }
+            if i < b.digits.count {
+                ans[i] += b.digits[i]
+            }
+        }
+        for i in 0..<possibleLen {
+            if i < possibleLen - 1 {
+                ans[i+1] += ans[i] / 10
+            }
+            ans[i] = ans[i] % 10
+        }
+        var len = possibleLen
+        while(len>0 && ans[len-1] == 0) {
+            len = len-1
+        }
+        
+        let ret = ans.prefix(len).reversed().reduce("", {$0 + String($1)})
+        return ret
+        
+    }
+}
